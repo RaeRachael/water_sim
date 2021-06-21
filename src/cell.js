fc8559b4015ffd63a5197b67c028aeee99bf5b24
Rachael Ewins <rachaelewins@hotmail.com>
2021-06-22 01:11:18 +0200

    builing out sediment aspect of a cell

--- src/cell.js
+++ src/cell.js
@@ -1,17 +1,14 @@
 class Cell {
   
-  position
-  cell_heights
   water_composition
 
-  constructor(position, heights) {
-    this.position = position
-    this.cell_heights = heights
+  constructor(water_height, rock_height, soil_height) {
+    this.water_abs_height = rock_height + water_height
     this.water_composition = {soil: 0, rock: 0}
   }
 
   display() {
-    switch (this.maxheight(this.cell_heights)) {
+    switch (this.maxheight()) {
       case "rock":
         return "grey"
       case "soil":
@@ -22,15 +19,58 @@ class Cell {
       
   }
 
-  maxheight(cell_heights) {
-    if (cell_heights.rock_height === cell_heights.soil_height 
-      && cell_heights.rock_height > cell_heights.water_height) {
-        return "rock"
+  maxheight() {
+    if (this.water_height < 0.01 && this.soil_height < 0.01) {
+      return "rock"
     }
     if (cell_heights.soil_height > cell_heights.water_height) {
       return "soil"
     }
     return "water"
+  }
+
+  getSpeed(cell1, cell2) {
+    var cell1_difference = (cell1.water_abs_height - this.water_abs_height) * this.passable(cell1)
+    var cell2_difference = (cell2.water_abs_height - this.water_abs_height) * this.passable(cell2)
+    return (cell1_difference - cell2_difference)/10
+  }
+
+  passable(cell) {
+    if(cell.water_height < 0.01 && cell.rock_height > this.water_abs_height) {
+      return 0
+    }
+    return 1
+  }
+
+  sedimentCollectDrop(xSpeed, ySpeed) {
+    var speed = (xSpeed**2 + ySpeed**2)**0.5
+    var carryCapacity = this.calculateCarryCapacity(speed)
+    if (this.water_composition.rock < carryCapacity.rock_min) {
+      this.rock_height -= (carryCapacity.rock_min - this.water_composition.rock) * this.water_height
+      this.water_composition.rock = carryCapacity.rock_min
+    }
+    if (this.water_composition.soil < carryCapacity.soil_min) {
+      change_soil_height -= (carryCapacity.rock_min - this.water_composition.rock) * this.water_height
+      if(this.soil_height < change_soil_height) {
+        this.soil_height = 0
+        this.water_composition += change_soil_height/this.water_height
+      } else {
+        this.soil_height -= change_soil_height
+        this.water_composition.rock = carryCapacity.rock_min
+      }
+    }
+    if (this.water_composition.rock > carryCapacity.rock_max) {
+      this.rock_height += (this.water_composition.rock - carryCapacity.rock_max) * this.water_height
+      this.water_composition.rock = carryCapacity.rock_max
+    }
+    if (this.water_composition.soil > carryCapacity.soil_max) {
+      this.soil_height += (this.water_composition.soil - carryCapacity.soil_max) * this.water_height
+      this.water_composition.soil = carryCapacity.soil_max
+    }
+  }
+
+  calculateCarryCapacity(speed) {
+    return {rock_min: speed/100, rock_max: speed/50, soil_min: speed/20, soil_max: speed/10}
   }
 }
 
--- src/simulation.js
+++ src/simulation.js
@@ -1,9 +1,10 @@
 const Cell = require('./src/cell.js')
 
 var cellMap = []
-for(var i = 0; i < 61; i++) {
-  for(var j = 0; j < 41; j ++) {
-    cellMap.push(new Cell({x: i, y: j}, {rock_height: i, soil_height: j/4, water_height: i * j /100 }))
+for(var i = 0; i < 601; i++) {
+  cellMap[i] = []
+  for(var j = 0; j < 401; j ++) {
+    cellMap[i].push(new Cell({rock_height: i/60, soil_height: j/40, water_height: i * j /10000 }))
   }
 }
 
@@ -12,15 +13,35 @@ const simultionBox = document.getElementById('simulation-box')
 drawSimulation(simultionBox, cellMap)
 
 function drawSimulation(simulationBox, cellMap) {
-  cellMap.forEach(cell => {
-    displayOneTile(simulationBox, cell)
-  });
+  for(var x = 0; x < cellMap.length -1; x++) {
+    for(var y = 0; y < cellMap[x].length -1; y++) {
+      displayOneTile(simulationBox, cellMap[x][y], x, y)
+    }
+  }
 }
 
-function displayOneTile(simulationBox, cell) {
+function displayOneTile(simulationBox, cell, x, y) {
   const cellDisplay = document.createElement('div')
-  cellDisplay.style.gridRowStart = cell.position.y
-  cellDisplay.style.gridColumnStart = cell.position.x
+  cellDisplay.style.gridRowStart = y+1
+  cellDisplay.style.gridColumnStart = x+1
   cellDisplay.style.backgroundColor = cell.display()
   simulationBox.appendChild(cellDisplay)
+}
+
+function runSimulation(simultionBox, cellMap) {
+  drawSimulation(simultionBox, cellMap)
+  updateSimulation(cellMap)
+}
+
+function updateSimulation(cellMap) {
+  for(var x = 1; x < cellMap.length -2; x++) {
+    for(var y = 1; y < cellMap[x].length -2; y++) {
+      var xSpeed = cellMap[x][y].getSpeed(cellMap[x-1][y], cellMap[x+1][y])
+      var ySpeed = cellMap[x][y].getSpeed(cellMap[x][y-1], cellMap[x][y+1])
+      cellMap[x][y].sedimentCollectDrop(xSpeed, ySpeed)
+      cellMap[x][y].transport(cellMap[x-1][y], cellMap[x+1][y], xSpeed) // to code transport
+      cellMap[x][y].transport(cellMap[x][y-1], cellMap[x][y+1], ySpeed)
+      displayOneTile(simulationBox, cellMap[x][y], x, y)
+    }
+  }
 }
